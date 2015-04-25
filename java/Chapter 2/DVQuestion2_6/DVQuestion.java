d67535046358a689c83af0ef62cef4f9433b70e2
Doug Voss <douglas.c.voss@gmail.com>
2015-04-25 12:48:18 -0600

    implemented Floyd's tortoise and hare algorithm and ctci extension of it for finding the start of a linked list loop

--- java/Chapter 2/DVQuestion2_6/DVQuestion.java
+++ java/Chapter 2/DVQuestion2_6/DVQuestion.java
@@ -6,7 +6,8 @@ import java.util.HashSet;
 public class DVQuestion {
 
 	public static final int randSeed = 125;
-	public static Random r = new Random(randSeed);
+	//public static Random r = new Random(randSeed);
+	public static Random r = new Random();
 	public static boolean bigEndian = true;
 	
 	public static int randInt(int min, int max) {
@@ -36,7 +37,7 @@ public class DVQuestion {
 			cur = cur.next;
 		}
 		circNode = cur;
-		for (int i=circStartIndex; i<size; i++) {
+		for (int i=circStartIndex; i<size-1; i++) {
 			cur.next = new DVLinkedListNode(randInt(0,10), null);
 			cur = cur.next;
 		}
@@ -62,7 +63,7 @@ public class DVQuestion {
 		System.out.print("\n");
 	}
 	
-	public static int detectCircleStartNode(DVLinkedListNode n)
+	/*public static int detectCircleStartNode(DVLinkedListNode n)
 	{
 		HashSet<DVLinkedListNode> nodeSet = new HashSet<DVLinkedListNode>(); 
 		int circStartI = 0;
@@ -72,6 +73,38 @@ public class DVQuestion {
 			n=n.next;
 		}
 		return circStartI;
+	}*/
+	
+	public static int detectCircleStartNode(DVLinkedListNode n)
+	{
+		DVLinkedListNode head = n;
+		DVLinkedListNode slowPtr = n;
+		DVLinkedListNode fastPtr = n;
+		int k=1;
+		
+		if (slowPtr.next == null || fastPtr.next.next == null) {
+			return -1;
+		} else {
+			do {
+				slowPtr = slowPtr.next;
+				fastPtr = fastPtr.next.next;
+				
+				if (slowPtr == null || fastPtr == null) {
+					return -1;
+				}
+			} while (slowPtr != fastPtr);
+			// slow and fast ptr should now be "k" nodes away from the node at the start of the loop
+			// through some fancy derivation
+			// the definition of k is how many nodes from head the loop starts
+			// so if we advance the slowPtr and the head ptr then when they collide is the start of the loop
+			
+			while (head != slowPtr) {
+				head = head.next;
+				slowPtr = slowPtr.next;
+				k++;
+			}
+			return k;
+		}
 	}
 	
 	public static DVLinkedListNode getNodeInLinkedList(DVLinkedListNode n, int destIndex)
@@ -83,13 +116,14 @@ public class DVQuestion {
 	}
 
 	public static void main (String [] args) {
-		DVLinkedListNode n1=makeRandomCircularLinkedList(10, 5);
+		int k = randInt(3,4); // k is how many nodes before loop starts
+		int loopLen = randInt(3,4);
+		DVLinkedListNode n1=makeRandomCircularLinkedList(k+loopLen, k);
+		System.out.printf("k=%d; loopLen=%d; ", k, loopLen);
 		System.out.printf("n1: ");
-		printLinkedList(n1, 20);
+		printLinkedList(n1, k+(loopLen*2));
 		int circStartIndex = detectCircleStartNode(n1);
 		DVLinkedListNode startCircNode = getNodeInLinkedList(n1, circStartIndex);
 		System.out.printf("circle start node is node %d which is value %d\n", circStartIndex, startCircNode.data);
-		
-		
 	}
 }
